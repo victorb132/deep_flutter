df84a40756a1438c8792afee40b075cc0b2b7187
Victor Novais <victornovais@Victors-Laptop.local>
2024-06-05 13:00:51 -0300

    feat/change-project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:deep_flutter/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-import 'pages/home_page.dart';
-
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-      useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.white)
-          .copyWith(background: Colors.black),
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white)
+            .copyWith(background: Colors.black),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
     ),
-    debugShowCheckedModeBanner: false,
-    home: const HomePage(),
-  ));
+  );
 }
